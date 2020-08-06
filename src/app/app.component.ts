e94ebb3555b732ec5164fffc0f5d5c3eb71b4eef
tarek1500 <tarek_mohammed2@yahoo.com>
2020-08-06 23:52:26 +0200

    Add main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 	title = 'luxury';
+	mobileQuery: MediaQueryList;
+	mobileQueryListener: () => void;
+
+	constructor(media: MediaMatcher, changeDetectorRef: ChangeDetectorRef) {
+		this.mobileQuery = media.matchMedia('(max-width: 600px)');
+		this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+		this.mobileQuery.addListener(this.mobileQueryListener);
+	}
+
+	ngOnInit(): void { }
+
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this.mobileQueryListener);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +19,12 @@ import { AppComponent } from './app.component';
 		BrowserModule,
 		BrowserAnimationsModule,
 		ReactiveFormsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+		MatButtonModule,
+		MatIconModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
